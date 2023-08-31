ee66f9b8538b5eb8d9ef2eafb1376f666b350600
Vladisdev <vladisdlavpopov.dev@gmail.com>
2023-08-31 17:35:28 +0300

    fix: types error

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -2,14 +2,15 @@
 
 import { FC } from 'react'
 import styles from './Input.module.scss'
-import { FieldValues, UseFormRegister } from 'react-hook-form';
+import { UseFormRegister } from 'react-hook-form';
+import { FormValues, FormValuesBig } from '@/types';
 
 interface InputProps {
   type: string
   placeholder?: string
   id?: string
   fieldName: string
-  register: UseFormRegister<FieldValues>
+  register: UseFormRegister<FormValues | FormValuesBig>
 }
 
 export const Input: FC<InputProps> = ({ type, placeholder, id, fieldName, register }) => {
