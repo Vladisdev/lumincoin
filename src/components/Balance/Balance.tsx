4a5e677258fd36ecede34fde94ee84578159fb6c
Vladisdev <vladisdlavpopov.dev@gmail.com>
2023-09-11 15:42:19 +0300

    users data in aside

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,10 +1,16 @@
+'use client'
+
+import { RootState } from '@/redux/store'
+import { useSelector } from 'react-redux'
 import styles from './Balance.module.scss'
 
 export const Balance = () => {
+  const user = useSelector((state: RootState) => state.user)
+
   return (
     <div className={styles.balance}>
       <span className={styles.balance__text}>Баланс:</span>
-      <span className={styles.balance__value}>500$</span>
+      <span className={styles.balance__value}>{user.balance}$</span>
     </div>
   )
 }
--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import styles from './User.module.scss'
+'use client'
+
+import { RootState } from '@/redux/store'
+import { useSelector } from 'react-redux'
 import { Icon } from '../UI/Icons/Icons'
+import styles from './User.module.scss'
 
 export const User = () => {
+  const user = useSelector((state: RootState) => state.user)
+
   return (
     <div className={styles.user}>
-      <Icon name="name"/>
-      <div className={styles.user__name}>Test Name</div>
+      <Icon name='name' />
+      <div className={styles.user__name}>{user.name}</div>
     </div>
   )
 }
