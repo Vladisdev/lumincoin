cf66f528aa86c81b9fca4ba0122011abe888e8f6
Vladisdev <vladisdlavpopov.dev@gmail.com>
2023-09-11 13:50:24 +0300

    refactor: remove unnecessary div

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <>
       <AppProgressBar height='4px' color='#0D6EFD' shallowRouting />
       {children}
-    </div>
+    </>
   )
 }
